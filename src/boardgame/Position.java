ea73282400bfff40923fcd4d1919002b4511d8f3
Karlos Elyas <karloselyas2@gmail.com>
2021-01-24 16:26:23 -0300

    Chess layer and printing the board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -12,6 +12,22 @@ public class Position {
 		
 	}
 	
+	public Integer getRow() {
+		return row;
+	}
+
+	public void setRow(Integer row) {
+		this.row = row;
+	}
+
+	public Integer getColumn() {
+		return column;
+	}
+
+	public void setColumn(Integer column) {
+		this.column = column;
+	}
+
 	@Override
 	public String toString() {
 		StringBuilder sb = new StringBuilder();
